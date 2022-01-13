96789bc9c8b3f31b8f30b6966621ecca87ecc778
StephenMakenziWaweru <stevomakenzi@gmail.com>
2022-01-13 11:42:52 +0300

    removed .

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,7 +10,7 @@ def do_pack():
     date = datetime.now().strftime("%Y%m%d%H%M%S")
     filename = "versions/web_static_{}.tgz".format(date)
     if not os.path.exists("versions/"):
-        os.mkdir.("versions/")
+        os.mkdir("versions/")
     with tarfile.open(filename, "w:gz") as tar:
         tar.add("web_static", arcname=os.path.basename("web_static"))
     if os.path.exists(filename):
