f671cc3931ff89ce0883f5074229a2c497ace34f
StephenMakenziWaweru <stevomakenzi@gmail.com>
2022-01-13 12:16:17 +0300

    2

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Deploys compressed files to webservers"""
+from fabric.api import *
+import os
+
+# define hosts and user
+env.hosts = [
+        "34.139.26.238",
+        "18.207.4.175",
+        ]
+env.user = 'ubuntu'
+
+
+def do_deploy(archive_path):
+    """Deploys.gz archive to web servers"""
+    if not os.path.exists(archive_path):
+        return False
+    filename = os.path.basename(archive_path)
+    filename_ext = filename.split(".")[0]
+    new_dir = "/data/web_static/releases/" + filename_ext
+    put(archive_path, '/tmp/')
+    run(f"mkdir -p {new_dir}")
+    run(f"tar -xzf /tmp/ {filename} -C {newdir}")
+    
+    run(f"rm /tmp/ {filename}")
+    run(f"mv {newdir} /web_static/* {new_dir}")
+    run(f"rm -rf {new_dir} /web_static")
+    run(f"rm -rf /data/web_static/current")
+    run(f"ln -s {new_dir} /data/web_static/current")
+
+    return True
