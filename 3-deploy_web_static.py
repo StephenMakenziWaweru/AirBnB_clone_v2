b71d65a0e8e828e85b0fb60127fcd3860670f39f
StephenMakenziWaweru <stevomakenzi@gmail.com>
2022-01-13 14:36:47 +0300

    added parenthesis

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -34,14 +34,14 @@ def do_deploy(archive_path):
     filename_ext = filename.split(".")[0]
     new_dir = "/data/web_static/releases/" + filename_ext
     put(archive_path, '/tmp/')
-    run("mkdir -p {}".format(new_dir)
+    run("mkdir -p {}".format(new_dir))
     run("tar -xzf /tmp/{} -C {}".format(filename, new_dir))
 
     run("rm /tmp/{}".format(filename))
-    run("mv {}/web_static/* {}".format(new_dir, new_dir)
+    run("mv {}/web_static/* {}".format(new_dir, new_dir))
     run("rm -rf {}/web_static".format(new_dir))
     run("rm -rf /data/web_static/current")
-    run("ln -s {} /data/web_static/current".format(new_dir)
+    run("ln -s {} /data/web_static/current".format(new_dir))
 
     return True
 
