565df79e99f8ba2c3665e974f8fd78e91991024b
StephenMakenziWaweru <stevomakenzi@gmail.com>
2022-01-13 11:36:07 +0300

    1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Fabfile to create a .tgz archive"""
-import fabric.api
+from fabric.api import local
 from datetime import datetime
 import os
 
@@ -8,10 +8,10 @@ import os
 def do_pack():
     """creates a .tgz archive"""
     date = datetime.now().strftime("%Y%m%d%H%M%S")
-    filename = f"versions/web_static_{date}.tgz"
+    filename = "versions/web_static_{}.tgz".format(date)
     if not os.path.exists("/versions/"):
         local("mkdir versions/")
-    local("tar -cvzf {} web_static".format(file_name))
+    local("tar -cvzf {} web_static".format(filename))
     if os.path.exists(filename):
         return filename
     else:
