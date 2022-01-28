8aed75cee6a9fd61e9e8ab2c94ec6cedcdfadf4d
StephenMakenziWaweru <stevomakenzi@gmail.com>
2022-01-28 12:32:29 +0300

    task 1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module - script that starts a Flask web application"""
+from email.policy import strict
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Handles the root url"""
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Handles hbnb route"""
+    return 'HBNB'
+
+if __name__ == '__main__':
+    app.run("0.0.0.0", 5000)
